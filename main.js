57ebe394b4ef6d8c521dd875bb02d4e23885ce15
unknown <dzenohaskic89@gmail.com>
2022-01-31 22:27:26 +0100

    movies VII

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let dataObject;
+let firstPage = document.querySelector('#first-page');
+let secondPage = document.querySelector('#second-page');
+let views = document.querySelectorAll('.view');
+let id;
 let hollywood = document.querySelector('hollywood');
 
 
@@ -7,23 +11,15 @@ fetch('main.json')
 .then(data => {
   dataObject = data;
   
-
+document.body.style.backgroundImage = "url('https://occ-0-6146-2705.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABd6RGXWbIoOHL3daqY8inmYcduPOmeukBFfOpzMJA0yh0ogdbGvekRFLgP1eyZBDFO2Igi5gLPKlusDO9AnqbE_UCIcA.jpg?r=6a0%22')";
 })
 
-movie = { 
-  "id": 1, 
-  "name": "Don't Look Up", 
-  "year": "2021" ,
-  "duration":"2h18",
-  "background-image":"https://occ-0-6146-2705.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABd6RGXWbIoOHL3daqY8inmYcduPOmeukBFfOpzMJA0yh0ogdbGvekRFLgP1eyZBDFO2Igi5gLPKlusDO9AnqbE_UCIcA.jpg?r=6a0%22)",
-  "description":"Two astronomers go on a media tour to warn humankind of a planet-killing comet hurtling toward Earth. The response from a distracted world: Meh",
-  "actors":"Leonardo DiCaprio, Jennifer Lawrence, Meryl Streep",
-"trailer":"https://www.youtube.com/watch?v=Pj0wz7zu3Ms&ab_channel=Netflix"
-};
 
-if( movie.id === 1) {
-document.body.style.backgroundImage = "url('https://occ-0-6146-2705.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABd6RGXWbIoOHL3daqY8inmYcduPOmeukBFfOpzMJA0yh0ogdbGvekRFLgP1eyZBDFO2Igi5gLPKlusDO9AnqbE_UCIcA.jpg?r=6a0%22')";
-}
+
+//document.body.style.backgroundImage = "url('https://occ-0-6146-2705.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABd6RGXWbIoOHL3daqY8inmYcduPOmeukBFfOpzMJA0yh0ogdbGvekRFLgP1eyZBDFO2Igi5gLPKlusDO9AnqbE_UCIcA.jpg?r=6a0%22')";
+
+
+
 
 
 
